549c8a93d88b894f4c3ff544b7914475015e8a02
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-12-10 12:26:35 +0330

    FP refactoring in process

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //https://www.codewars.com/kata/56882731514ec3ec3d000009/train/javascript
 //Video link: https://www.bing.com/videos/riverview/relatedvideo?&q=connect+four+rules+&&mid=A4E0C4A8044145677E23A4E0C4A8044145677E23&&FORM=VRDGAR
 
-class FindWinner {
+class WinnerFinder {
   constructor() {
     this.board = ["", "", "", "", "", "", ""];
     this.colorCodes = { Yellow: 1, Red: 0 };
@@ -33,7 +33,43 @@ class FindWinner {
     if (/0{4}|1{4}/.test(rightToLeftDiagonal)) return color;
   }
 
-  debugging() {
+  traverse() {
+    this.checkDiagonalLeftToRight();
+    this.checkDiagonalRightToLeft();
+  }
+
+  spotFinder() {
+    let [X_index] = this._colData;
+    const currentCol = this.board[X_index];
+    const Y_index = currentCol.length - 1;
+    this._spotFinderData = [X_index, Y_index];
+    this._startSpots = [
+      X_index - Y_index < 0 ? 0 : X_index - Y_index,
+      Y_index - X_index < 0 ? 0 : Y_index - X_index,
+      X_index + Y_index > 6 ? 6 : X_index + Y_index,
+      Y_index - (6 - X_index) < 0 ? 0 : Y_index - (6 - X_index)
+    ];
+  }
+  
+  diagonalCheck() {
+    this.spotFinder();
+    this.traverse();
+    this.logs()
+  }
+  
+  crossCheck() {
+    let [X_index, col, color] = this._colData;
+    const currentCol = this.board[X_index];
+    const Y_index = currentCol.length - 1;
+    let [x, y] = ["", currentCol];
+    for (let i = 0; i < this.board.length; i++) {
+      //build x:
+      x += this.board[i][Y_index] ?? "*";
+    }
+    return (/0{4}|1{4}/.test(x) || /0{4}|1{4}/.test(y)) && color
+  }
+
+  logs() {
     const [leftXstartSpot, leftYstartSpot, rightXstartSpot, rightYstartSpot] =
       this._startSpots;
     const [X_index, Y_index] = this._spotFinderData;
@@ -46,7 +82,8 @@ class FindWinner {
       "X_index>",
       X_index,
       "Y_index>",
-      Y_index
+      Y_index,
+      "color>", color
     );
     console.log(
       ".".repeat(3),
@@ -68,43 +105,7 @@ class FindWinner {
     );
     console.log("_".repeat(28));
   }
-
-  traverse() {
-    this.checkDiagonalLeftToRight();
-    this.checkDiagonalRightToLeft();
-  }
-
-  spotFinder() {
-    const [X_index, Y_index] = this._spotFinderData;
-    this._startSpots = [
-      X_index - Y_index < 0 ? 0 : X_index - Y_index,
-      Y_index - X_index < 0 ? 0 : Y_index - X_index,
-      X_index + Y_index > 6 ? 6 : X_index + Y_index,
-      Y_index - (6 - X_index) < 0 ? 0 : Y_index - (6 - X_index)
-    ];
-  }
-
-  diagonalCheck() {
-    let [X_index] = this._colData;
-    const currentCol = this.board[X_index];
-    const Y_index = currentCol.length - 1;
-    this._spotFinderData = [X_index, Y_index];
-    this.spotFinder();
-    this.traverse();
-    this.debugging()
-  }
-
-  crossCheck() {
-    let [X_index, col, color] = this._colData;
-    const currentCol = this.board[X_index];
-    const Y_index = currentCol.length - 1;
-    let [x, y] = ["", currentCol];
-    for (let i = 0; i < this.board.length; i++) {
-      x += this.board[i][Y_index] ?? "*";
-    }
-    if (/0{4}|1{4}/.test(x) || /0{4}|1{4}/.test(y)) return color;
-  }
-
+ 
   set moves(piecesPositionList) {
     const keyCol = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6 };
     for (let i = 0; i < piecesPositionList.length; i++) {
@@ -112,20 +113,18 @@ class FindWinner {
       const X_index = keyCol[col];
       this.board[X_index] += this.colorCodes[color];
       this._colData = [X_index, col, color];
-      this._traverseLeftToRightData = [];
       this._result = this.crossCheck() || this.diagonalCheck() || this.checkDiagonalLeftToRight() || this.checkDiagonalRightToLeft();
-      if (this._result === undefined) this._result === 'Draw'
       if (this._result) break;
     }
   }
 
   get result() {
-    return this._result;
+    return this._result
   }
 }
 
 const whoIsWinner = (piecesPositionList) => {
-  const winner = new FindWinner();
+  const winner = new WinnerFinder();
   winner.moves = piecesPositionList;
   return winner.result ?? 'Draw';
 };
